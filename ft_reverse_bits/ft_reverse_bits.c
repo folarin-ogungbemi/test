2902653b7b2e62920c04c840b7af0c74e08eb29d
folarin-ogungbemi <ftogungbemi@gmai.com>
2025-08-28 11:42:01 +0200

    rev bit

---
+++ ft_reverse_bits/ft_reverse_bits.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+
+unsigned char	reverse_bits(unsigned char octet)
+{
+	unsigned int	i;
+	unsigned char	c;
+
+	c = 0;
+	i = 8;
+	while (i--)
+	{
+		c = (c << 1) | (octet & 1);
+		octet = octet >> 1;
+	}
+	return (c);
+}
+
+int	main(void)
+{
+	int				i;
+	unsigned char	bit;
+	unsigned char	normal_bit;
+	unsigned char	rev_bit;
+
+	bit = 0;
+	rev_bit = reverse_bits(5);
+	normal_bit = 5;
+	i = 8;
+	printf("\n---------------\n");
+	while (i--)
+	{
+		bit = ((normal_bit >> i) & 1) + 48;
+		write(1, &bit, 1);
+	}
+	printf("\n---------------\n");
+	i = 8;
+	while (i--)
+	{
+		bit = ((rev_bit >> i) & 1) + 48;
+		write(1, &bit, 1);
+	}
+	printf("\n---------------\n");
+}
